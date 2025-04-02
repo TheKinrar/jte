f6eed5620a52082f16524a20d7252d0534fc4529
Amaury Rousseau <kinrar@kinrar.io>
2025-04-02 07:50:12 +0200

    wip

--- test/jte-runtime-cp-test-gradle-kotlin-convention/build.gradle.kts
+++ test/jte-runtime-cp-test-gradle-kotlin-convention/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.1.20"
     id("gg.jte.gradle") version("3.1.17-SNAPSHOT")
 }
 
--- test/kte-runtime-test-gradle-kotlin-convention/build.gradle.kts
+++ test/kte-runtime-test-gradle-kotlin-convention/build.gradle.kts
@@ -1,6 +1,6 @@
 
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.1.20"
     id("gg.jte.gradle") version("3.1.17-SNAPSHOT")
 }
 
